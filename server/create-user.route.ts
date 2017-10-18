0188011e9649c31059cce58e5fe3ea0374d5b1a3
armenh <avarmenhg@gmail.com>
2017-10-18 14:38:09 -0400

    cookies added

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -5,6 +5,7 @@ import {USERS} from './database-data';
 import * as argon2 from 'argon2';
 import {validatePassword} from './password-validation';
 import { randomBytes } from './security.utils';
+import { sessionStore } from './session-store';
 
 
 
@@ -27,5 +28,7 @@ async function createUserAndSession(res: Response, credentials ) {
   const user = db.createUser(credentials.email, passwordDigest);
   const sessionId = await randomBytes(32).then(bytes => bytes.toString());
   console.log('sessionId', sessionId);
+  sessionStore.createSession(sessionId, user);
+  res.cookie('SESSIONID', sessionId);
   res.status(200).json({id: user.id, email: user.email});
 }
